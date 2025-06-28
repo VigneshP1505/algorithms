09a733e26cdc5147106a51aac759c23141333e3a
Vignesh Pugazhendhi <>
2025-06-28 15:20:02 -0400

    basics of cpp

---
+++ datatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=10;
+    long long y=150000000;
+    string s;
+    getline(cin, s);
+    cin>>s;
+    cout<<"value of s: %s"<<s;
+    return 1;
+}
---
+++ input.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cin >> x >> y;
+    cout << "Value of x:" << x << " and Value of y:" << y;
+    return 0;
+}

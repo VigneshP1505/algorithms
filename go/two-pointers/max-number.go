ec04b635858bdf4b439c53c77f8781930d29e5b4
Vignesh Pugazhendhi <>
2026-05-21 00:54:33 -0400

    fixed stack pop condition

--- go/two-pointers/max-number.go
+++ go/two-pointers/max-number.go
@@ -5,7 +5,7 @@ func _maxNumber(a []int, k int) int {
 	stack := []int{}
 	for i := 0; i < len(a); i++ {
 		remaining := len(a) - i - 1
-		for len(stack) > 0 && stack[len(stack)-1] < a[i] && len(stack)+remaining >= k {
+		for len(stack) > 0 && stack[len(stack)-1] < a[i] && len(stack)+remaining > k {
 			stack = stack[:len(stack)-1]
 		}
 		if len(stack) < k {

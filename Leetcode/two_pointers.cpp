92d759dc79714610aacbb002a87efd2c62b8d7fc
Vignesh Pugazhendhi <>
2026-08-06 22:06:02 -0400

    array sorting parity

--- Leetcode/two_pointers.cpp
+++ Leetcode/two_pointers.cpp
@@ -260,3 +260,13 @@ bool backspace_O1_Space(string s, string t){
     }
     return true;
 }
+
+
+vector<int> sortArrayByParity(vector<int>& nums){
+    for(int i=0,j=0;j<nums.size();j++){
+        if(nums[j]%2==0){
+            swap(nums[i++],nums[j]);
+        }
+    }
+    return nums;
+}

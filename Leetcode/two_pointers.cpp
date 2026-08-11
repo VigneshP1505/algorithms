bd358e60d6f5e994fc8099b4b7546e3ee027ae66
Vignesh Pugazhendhi <>
2026-08-10 23:03:58 -0400

    sort squared array

--- Leetcode/two_pointers.cpp
+++ Leetcode/two_pointers.cpp
@@ -297,3 +297,19 @@ bool longPressedName(string name, string typed){
               return false;
       return i == m;
 }
+
+
+vector<int> squareAndSort(vector<int>& nums){
+    vector<int> result(nums.size());
+    int p1=0,p2=static_cast<int>(nums.size()-1);
+    for(int i=nums.size()-1;i>=0;i--){
+        if(abs(nums[p1])>abs(nums[p2])){
+            result[i]=nums[p1]*nums[p1];
+            p1++;
+        } else{
+            result[i]=nums[p2]*nums[p2];
+            p2--;
+        }
+    }
+    return result;
+}
